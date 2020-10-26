7afafbe02520fd4b466d32af6148995e231ab98a
Cameron Daniel <cam.daniel@gmail.com>
2020-10-26 10:03:26 +0100

    wip

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
-const fs = require('fs');
-const path = require('path');
-const ncc = require('@vercel/ncc');
+const deasync = require("deasync");
+const ncc = require("@vercel/ncc");
+const path = require("path");
 
 const extractTags = new RegExp(
   /(\((?<body>\w+)\)\s+)?\{(?<schemaDef>\w+)\}\s+(?<apidocTarget>\w+)/
 );
 
-async function extractFromTypescript(modulePath, target) {
-  const { code } = await ncc(modulePath);
-  const imported = eval(code);
+function extractFromTypescript(modulePath, target) {
+  let generated,
+    done = false;
+  ncc(modulePath).then(({ code }) => {
+    generated = code;
+    done = true;
+  });
 
-  return imported[target];
+  deasync.loopWhile(() => !done);
+
+  const exports = eval(generated);
+  console.log("EXPORTED => ", Object.keys(exports));
+
+  return exports[target];
 }
 
-async function extractFromJavascript(modulePath, target) {
+function extractFromJavascript(modulePath, target) {
   const imported = require(modulePath);
   return imported[target];
 }
 
 function convertScalarType(type) {
   switch (type) {
-    case 'string':
-      return 'String';
+    case "string":
+      return "String";
       break;
 
     default:
       console.warn(`!! DON'T KNOW HOW TO CONVERT: ${type}`);
-      return 'String';
+      return "String";
   }
 }
 
@@ -39,7 +48,7 @@ function buildParams(object, target) {
     const description =
       item.flags && item.flags.description
         ? item.flags.description
-        : 'No description';
+        : "No description";
     const content = `{${convertScalarType(item.type)}} ${description}`;
 
     // console.log(`${key} => `, item);
@@ -54,10 +63,11 @@ function buildParams(object, target) {
   return params;
 }
 
-async function parserSchemaElements(elements, element, block, filename) {
+function parserSchemaElements(elements, element, block, filename) {
+  // console.log(`* Processing ${element.sourceName}`);
   // We only care about apiJoiSchema...
-  if (element.name !== 'apijoischema') return elements;
-  console.log('START ==> ', elements);
+  if (element.name !== "apijoischema") return elements;
+  // console.log("START ==> ", elements);
 
   const filePath = path.parse(filename);
 
@@ -69,39 +79,39 @@ async function parserSchemaElements(elements, element, block, filename) {
   // Remove the pseudo-element, we're going to build up a new set and
   // add those to `elements` instead
   elements.pop();
-  const elementsLen = elements.length;
 
   // Resolve the filename to a module path and import everything from the target
   const absModulePath = [process.cwd(), filename].join(path.sep);
   let validator;
   switch (filePath.ext) {
-    case '.js':
-    case '.jsx':
-      validator = await extractFromJavascript(absModulePath, schemaDef);
+    case ".js":
+    case ".jsx":
+      validator = extractFromJavascript(absModulePath, schemaDef);
       break;
 
-    case '.ts':
-    case '.tsx':
-      validator = await extractFromTypescript(absModulePath, schemaDef);
+    case ".ts":
+    case ".tsx":
+      validator = extractFromTypescript(absModulePath, schemaDef);
       break;
 
     default:
       throw new Error(`apidoc-plugin-joi: unsupported filetype ${path.ext}`);
   }
 
+  console.log("Validator -> ", validator);
+
   const description = validator.describe();
   const newParams = buildParams(description.keys, apidocTarget);
 
-  // Horrific hack because of async
-  elements.splice(elementsLen, 0, newParams);
-  console.log('AFTER ==>  ', elements);
-  return elements;
+  const final = [...elements, ...newParams];
+  console.log("FINAL => ", final);
+  return final;
 }
 
 let app = {};
 module.exports = {
   init: function (_app) {
     app = _app;
-    app.addHook('parser-find-elements', parserSchemaElements, 10);
+    app.addHook("parser-find-elements", parserSchemaElements, 10);
   },
 };
